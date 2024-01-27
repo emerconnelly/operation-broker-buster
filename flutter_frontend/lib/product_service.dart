a4e8ae8d7efde48283347d447dbe5767885af296
Emer Connelly <emer.connelly@wolterskluwer.com>
2024-01-26 21:46:17 -0600

    flutter init

---
+++ flutter_frontend/lib/product_service.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_frontend/product_model.dart';
+
+class ProductService {
+  final String productsURL = 'http://localhost:50000/products';
+  final Dio dio = Dio();
+
+  ProductService();
+
+  Future<List<Product>> getProducts() async {
+    late List<Product> products;
+    try {
+      final res = await dio.get(productsURL);
+
+      products = res.data['products']
+          .map<Product>(
+            (item) => Product.fromJson(item),
+          )
+          .toList();
+    } on DioError catch (e) {
+      products = [];
+    }
+
+    return products;
+  }
+}
